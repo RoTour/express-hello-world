b128914b3dd1b87a06ebcdfd582a1cc3f5134686
Robin TOURNÉ <robin.tourne@ynov.com>
2023-09-15 09:50:47 +0200

    feat:

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // hello world express app
 const express = require('express')
+const crypto = require('crypto')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/api', (req, res) => {
+  res.json({message: 'Hello World!', code: crypto.randomUUID()})
 })
 
 app.listen(port, () => {
